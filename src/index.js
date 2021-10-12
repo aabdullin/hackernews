7cd3c4f7c6223b5b50d8c4030298d02df868d493
Artur <rutrika@gmail.com>
2021-10-11 21:41:59 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './index.css'
 import 'font-awesome/css/font-awesome.min.css';
+import imain from './Images/imain.jpeg';
+import isub1 from './Images/isub1.jpeg';
+import isub2 from './Images/isub2.jpeg';
+import isub3 from './Images/isub3.jpeg';
+import tmain from './Images/tmain.jpeg';
+import tsub1 from './Images/tsub1.jpeg';
+import tsub2 from './Images/tsub2.jpeg';
+import tsub3 from './Images/tsub3.jpeg';
 
 
 function PinterestCard ( {testFile} ) {
     return (
         <div className="card">
-
                 <div className="title"> {testFile.title} </div>
-                <div className="subtitle">Aviation explorer</div>
-
+                <div className="subtitle"> - Beautiful country</div>
                 <div className="images">
-                    <img src={testFile.mainImg} alt="1" className="main" />
+                    <img src={testFile.mainImg} className="main"/>
+                    <div className="secondary"> 
+                        <img src={testFile.subImg1} className="sub_image"/>
+                        <img src={testFile.subImg2} className="sub_image"/>
+                        <img src={testFile.subImg3} className="sub_image"/>
+                    </div>
+                    <button className="button"> Follow </button>
                 </div>
         </div>
     )
@@ -43,42 +55,42 @@ const testCards = [
     {
         title: "Turkey",
         pins: 4850,
-        mainImg: require('./Images/tmain.jpeg'),
-        subImg1: require('./Images/tsub1.jpeg'),
-        subImg2: require('./Images/tsub2.jpeg'),
-        subImg3: require('./Images/tsub3.jpeg'),
+        mainImg: tmain,
+        subImg1: tsub1,
+        subImg2: tsub2,
+        subImg3: tsub3,
     },
     {
         title: "Israel",
         pins: 209,
-        mainImg: require('./Images/imain.jpeg'),
-        subImg1: require('./Images/isub1.jpeg'),
-        subImg2: require('./Images/isub2.jpeg'),
-        subImg3: require('./Images/isub3.jpeg'),
+        mainImg: imain,
+        subImg1: isub1,
+        subImg2: isub2,
+        subImg3: isub3,
     },
     {
-        title: "Ideas Repair",
+        title: "Jordan",
         pins: 4850,
-        mainImg: require('./Images/imain.jpeg'),
-        subImg1: require('./Images/isub1.jpeg'),
-        subImg2: require('./Images/isub2.jpeg'),
-        subImg3: require('./Images/isub3.jpeg'),
+        mainImg: tmain,
+        subImg1: tsub1,
+        subImg2: tsub2,
+        subImg3: tsub3,
     },
     {
-        title: "Navigation",
+        title: "Morocco",
         pins: 641,
-        mainImg: require('./Images/imain.jpeg'),
-        subImg1: require('./Images/isub1.jpeg'),
-        subImg2: require('./Images/isub2.jpeg'),
-        subImg3: require('./Images/isub3.jpeg'),
+        mainImg: imain,
+        subImg1: isub1,
+        subImg2: isub2,
+        subImg3: isub3,
     },
     {
-        title: "Great Aviation",
+        title: "UAE",
         pins: 209,
-        mainImg: require('./Images/imain.jpeg'),
-        subImg1: require('./Images/isub1.jpeg'),
-        subImg2: require('./Images/isub2.jpeg'),
-        subImg3: require('./Images/isub3.jpeg'),
+        mainImg: imain,
+        subImg1: isub1,
+        subImg2: isub2,
+        subImg3: isub3,
     },
     
 ];
